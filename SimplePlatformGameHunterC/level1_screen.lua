647d45029759d9359270d3a36331ab5084738231
Hunter Connolly <hunterconnolly@Amys-Air.lan>
2019-11-18 08:00:29 -0500

    a

--- SimplePlatformGameHunterC/level1_screen.lua
+++ SimplePlatformGameHunterC/level1_screen.lua
@@ -1,8 +1,8 @@
 -----------------------------------------------------------------------------------------
 --
 -- level1_screen.lua
--- Created by: Ms Raffin
--- Date: Nov. 22nd, 2014
+-- Created by: Hunter Connolly
+-- Date: Nov. 12, 2019
 -- Description: This is the level 1 screen of the game.
 -----------------------------------------------------------------------------------------
 
